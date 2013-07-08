3b6bb4b3742ec3095ed388b1d910dc7b627fa351
wode <1965799453@qq.com>
2013-07-08 20:33:13 +0800

    ui

---
+++ Summer-Java/src/summer/ui/DeleteTask.java
@@ -0,0 +1,77 @@
+package summer.ui;
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.GridBagLayout;
+import javax.swing.JLabel;
+import java.awt.GridBagConstraints;
+import java.awt.Font;
+import javax.swing.JButton;
+import java.awt.Insets;
+
+
+public class DeleteTask extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					DeleteTask frame = new DeleteTask();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public DeleteTask() {
+		setTitle("\u5220\u9664\u4EFB\u52A1");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 469, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		GridBagLayout gbl_contentPane = new GridBagLayout();
+		gbl_contentPane.columnWidths = new int[]{47, 162, 76, 10, 84, 0};
+		gbl_contentPane.rowHeights = new int[]{0, 77, 117, 0};
+		gbl_contentPane.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		gbl_contentPane.rowWeights = new double[]{0.0, 0.0, 0.0, Double.MIN_VALUE};
+		contentPane.setLayout(gbl_contentPane);
+		
+		JLabel lblNewLabel = new JLabel("\u786E\u8BA4\u8981\u5220\u9664**\u5417\uFF1F");
+		lblNewLabel.setFont(new Font("ËÎÌו", Font.PLAIN, 14));
+		GridBagConstraints gbc_lblNewLabel = new GridBagConstraints();
+		gbc_lblNewLabel.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel.gridx = 1;
+		gbc_lblNewLabel.gridy = 1;
+		contentPane.add(lblNewLabel, gbc_lblNewLabel);
+		
+		JButton btnNewButton = new JButton("\u662F");
+		btnNewButton.setFont(new Font("ËÎÌו", Font.PLAIN, 14));
+		GridBagConstraints gbc_btnNewButton = new GridBagConstraints();
+		gbc_btnNewButton.insets = new Insets(0, 0, 0, 5);
+		gbc_btnNewButton.gridx = 2;
+		gbc_btnNewButton.gridy = 2;
+		contentPane.add(btnNewButton, gbc_btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("\u5426");
+		btnNewButton_1.setFont(new Font("ËÎÌו", Font.PLAIN, 14));
+		GridBagConstraints gbc_btnNewButton_1 = new GridBagConstraints();
+		gbc_btnNewButton_1.gridx = 4;
+		gbc_btnNewButton_1.gridy = 2;
+		contentPane.add(btnNewButton_1, gbc_btnNewButton_1);
+	}
+
+}
