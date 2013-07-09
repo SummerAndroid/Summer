b5a311d65ed2c449057406b61e26b966b823a32a
Administrator <Administrator@211.87.230.10>
2013-07-09 15:00:01 +0800

    ui

---
+++ Summer-Java/src/summer/ui/AddType.java
@@ -0,0 +1,117 @@
+package summer.ui;
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.GridBagLayout;
+import javax.swing.JLabel;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.Color;
+
+
+public class AddType extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					AddType frame = new AddType();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public AddType() {
+		setTitle("\u6DFB\u52A0\u7C7B\u578B");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBackground(Color.WHITE);
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		GridBagLayout gbl_contentPane = new GridBagLayout();
+		gbl_contentPane.columnWidths = new int[]{75, 0, 162, 0, 81, 0};
+		gbl_contentPane.rowHeights = new int[]{38, 53, 50, 42, 0};
+		gbl_contentPane.columnWeights = new double[]{0.0, 0.0, 1.0, 0.0, 1.0, Double.MIN_VALUE};
+		gbl_contentPane.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		contentPane.setLayout(gbl_contentPane);
+		
+		JLabel label = new JLabel("\u8BBE\u5907\u7C7B\u578B\u7F16\u53F7\uFF1A");
+		GridBagConstraints gbc_label = new GridBagConstraints();
+		gbc_label.anchor = GridBagConstraints.EAST;
+		gbc_label.insets = new Insets(0, 0, 5, 5);
+		gbc_label.gridx = 1;
+		gbc_label.gridy = 1;
+		contentPane.add(label, gbc_label);
+		
+		textField = new JTextField();
+		GridBagConstraints gbc_textField = new GridBagConstraints();
+		gbc_textField.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField.insets = new Insets(0, 0, 5, 5);
+		gbc_textField.gridx = 2;
+		gbc_textField.gridy = 1;
+		contentPane.add(textField, gbc_textField);
+		textField.setColumns(10);
+		
+		JLabel lblNewLabel = new JLabel(" \u8BBE \u5907 \u540D \u79F0\uFF1A");
+		GridBagConstraints gbc_lblNewLabel = new GridBagConstraints();
+		gbc_lblNewLabel.anchor = GridBagConstraints.EAST;
+		gbc_lblNewLabel.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel.gridx = 1;
+		gbc_lblNewLabel.gridy = 2;
+		contentPane.add(lblNewLabel, gbc_lblNewLabel);
+		
+		textField_1 = new JTextField();
+		GridBagConstraints gbc_textField_1 = new GridBagConstraints();
+		gbc_textField_1.insets = new Insets(0, 0, 5, 5);
+		gbc_textField_1.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField_1.gridx = 2;
+		gbc_textField_1.gridy = 2;
+		contentPane.add(textField_1, gbc_textField_1);
+		textField_1.setColumns(10);
+		
+		JLabel lblNewLabel_1 = new JLabel("  \u6A21\u677F\u9879\u7F16\u53F7\uFF1A");
+		GridBagConstraints gbc_lblNewLabel_1 = new GridBagConstraints();
+		gbc_lblNewLabel_1.anchor = GridBagConstraints.EAST;
+		gbc_lblNewLabel_1.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel_1.gridx = 1;
+		gbc_lblNewLabel_1.gridy = 3;
+		contentPane.add(lblNewLabel_1, gbc_lblNewLabel_1);
+		
+		textField_2 = new JTextField();
+		GridBagConstraints gbc_textField_2 = new GridBagConstraints();
+		gbc_textField_2.insets = new Insets(0, 0, 5, 5);
+		gbc_textField_2.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField_2.gridx = 2;
+		gbc_textField_2.gridy = 3;
+		contentPane.add(textField_2, gbc_textField_2);
+		textField_2.setColumns(10);
+		
+		JButton button = new JButton("\u63D0\u4EA4");
+		GridBagConstraints gbc_button = new GridBagConstraints();
+		gbc_button.insets = new Insets(0, 0, 0, 5);
+		gbc_button.gridx = 3;
+		gbc_button.gridy = 4;
+		contentPane.add(button, gbc_button);
+	}
+
+}
---
+++ Summer-Java/src/summer/ui/ScanEquipment.java
@@ -0,0 +1,303 @@
+package summer.ui;
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.Icon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTable;
+import javax.swing.ListSelectionModel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.table.DefaultTableModel;
+
+import java.awt.GridBagLayout;
+import javax.swing.JLabel;
+
+import java.awt.Color;
+import java.awt.Container;
+import java.awt.Cursor;
+import java.awt.GridBagConstraints;
+import java.awt.Font;
+import java.awt.Insets;
+import javax.swing.JTextField;
+import javax.swing.JScrollPane;
+import javax.swing.ImageIcon;
+
+
+public class ScanEquipment extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
+	private JTextField textField_3;
+	private JTextField textField_4;
+	private JTextField textField_5;
+	private JTextField textField_6;
+	private JTextField textField_7;
+	private JTable table;
+	private JLabel lblNewLabel_8;
+	private JButton btnNewButton;
+	private JButton button;
+	private JButton button_1;
+	
+
+	
+	 private JButton btnNewButton_1;
+	 private Icon icon;
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ScanEquipment frame = new ScanEquipment();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public ScanEquipment() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 529, 570);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		GridBagLayout gbl_contentPane = new GridBagLayout();
+		gbl_contentPane.columnWidths = new int[]{75, 57, 0, 73, 0, 89, 148, 0};
+		gbl_contentPane.rowHeights = new int[]{54, 33, 35, 37, 34, 29, 197, 56, 0};
+		gbl_contentPane.columnWeights = new double[]{};
+		gbl_contentPane.rowWeights = new double[]{};
+		contentPane.setLayout(gbl_contentPane);
+		
+		
+		btnNewButton_1 = new JButton("New button");
+		GridBagConstraints gbc_btnNewButton_1 = new GridBagConstraints();
+		gbc_btnNewButton_1.gridheight = 2;
+		gbc_btnNewButton_1.gridwidth = 2;
+		gbc_btnNewButton_1.insets = new Insets(0, 0, 5, 5);
+		gbc_btnNewButton_1.gridx = 1;
+		gbc_btnNewButton_1.gridy = 1; 
+		Cursor cs=new Cursor(Cursor.HAND_CURSOR);
+		  
+		icon=new ImageIcon("/EquipmentManege/icon.jpg");
+	    btnNewButton_1=new JButton(new ImageIcon(ScanEquipment.class.getResource("/EquipmentManege/icon.jpg")));
+		btnNewButton_1.setBounds(20, 10, 80, 60);
+		//btnNewButton_1.addActionListener(this);
+		btnNewButton_1.setCursor(cs);
+		btnNewButton_1.setBackground(new Color(255,255,255));
+		btnNewButton_1.setBorder(null);
+		contentPane.add(btnNewButton_1, gbc_btnNewButton_1);
+		     
+		
+		lblNewLabel_8 = new JLabel("\u8BBE\u5907\u540D\u79F0\u3002\u3002\u3002");
+		lblNewLabel_8.setIcon(null);
+		lblNewLabel_8.setFont(new Font("宋体", Font.BOLD, 16));
+		GridBagConstraints gbc_lblNewLabel_8 = new GridBagConstraints();
+		gbc_lblNewLabel_8.gridwidth = 3;
+		gbc_lblNewLabel_8.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel_8.gridx = 3;
+		gbc_lblNewLabel_8.gridy = 0;
+		contentPane.add(lblNewLabel_8, gbc_lblNewLabel_8);
+		
+		
+		
+		JLabel lblNewLabel = new JLabel("   使用地：");
+		GridBagConstraints gbc_lblNewLabel = new GridBagConstraints();
+		gbc_lblNewLabel.anchor = GridBagConstraints.EAST;
+		gbc_lblNewLabel.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel.gridx = 5;
+		gbc_lblNewLabel.gridy = 1;
+		contentPane.add(lblNewLabel, gbc_lblNewLabel);
+		
+		textField_3 = new JTextField();
+		GridBagConstraints gbc_textField_3 = new GridBagConstraints();
+		gbc_textField_3.insets = new Insets(0, 0, 5, 0);
+		gbc_textField_3.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField_3.gridx = 6;
+		gbc_textField_3.gridy = 1;
+		contentPane.add(textField_3, gbc_textField_3);
+		textField_3.setColumns(10);
+		
+		JLabel lblNewLabel_1 = new JLabel("设备类型：");
+		GridBagConstraints gbc_lblNewLabel_1 = new GridBagConstraints();
+		gbc_lblNewLabel_1.anchor = GridBagConstraints.EAST;
+		gbc_lblNewLabel_1.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel_1.gridx = 5;
+		gbc_lblNewLabel_1.gridy = 2;
+		contentPane.add(lblNewLabel_1, gbc_lblNewLabel_1);
+		
+		textField_4 = new JTextField();
+		GridBagConstraints gbc_textField_4 = new GridBagConstraints();
+		gbc_textField_4.insets = new Insets(0, 0, 5, 0);
+		gbc_textField_4.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField_4.gridx = 6;
+		gbc_textField_4.gridy = 2;
+		contentPane.add(textField_4, gbc_textField_4);
+		textField_4.setColumns(10);
+		
+		JLabel lblNewLabel_7 = new JLabel("设备编号：");
+		GridBagConstraints gbc_lblNewLabel_7 = new GridBagConstraints();
+		gbc_lblNewLabel_7.anchor = GridBagConstraints.EAST;
+		gbc_lblNewLabel_7.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel_7.gridx = 0;
+		gbc_lblNewLabel_7.gridy = 3;
+		contentPane.add(lblNewLabel_7, gbc_lblNewLabel_7);
+		
+		textField = new JTextField();
+		GridBagConstraints gbc_textField = new GridBagConstraints();
+		gbc_textField.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField.gridwidth = 3;
+		gbc_textField.insets = new Insets(0, 0, 5, 5);
+		gbc_textField.gridx = 1;
+		gbc_textField.gridy = 3;
+		contentPane.add(textField, gbc_textField);
+		textField.setColumns(10);
+		
+		JLabel lblNewLabel_2 = new JLabel("     价格：");
+		GridBagConstraints gbc_lblNewLabel_2 = new GridBagConstraints();
+		gbc_lblNewLabel_2.anchor = GridBagConstraints.EAST;
+		gbc_lblNewLabel_2.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel_2.gridx = 5;
+		gbc_lblNewLabel_2.gridy = 3;
+		contentPane.add(lblNewLabel_2, gbc_lblNewLabel_2);
+		
+		textField_5 = new JTextField();
+		GridBagConstraints gbc_textField_5 = new GridBagConstraints();
+		gbc_textField_5.insets = new Insets(0, 0, 5, 0);
+		gbc_textField_5.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField_5.gridx = 6;
+		gbc_textField_5.gridy = 3;
+		contentPane.add(textField_5, gbc_textField_5);
+		textField_5.setColumns(10);
+		
+		JLabel lblNewLabel_6 = new JLabel("    编码：");
+		GridBagConstraints gbc_lblNewLabel_6 = new GridBagConstraints();
+		gbc_lblNewLabel_6.anchor = GridBagConstraints.EAST;
+		gbc_lblNewLabel_6.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel_6.gridx = 0;
+		gbc_lblNewLabel_6.gridy = 4;
+		contentPane.add(lblNewLabel_6, gbc_lblNewLabel_6);
+		
+		textField_1 = new JTextField();
+		GridBagConstraints gbc_textField_1 = new GridBagConstraints();
+		gbc_textField_1.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField_1.gridwidth = 3;
+		gbc_textField_1.insets = new Insets(0, 0, 5, 5);
+		gbc_textField_1.gridx = 1;
+		gbc_textField_1.gridy = 4;
+		contentPane.add(textField_1, gbc_textField_1);
+		textField_1.setColumns(10);
+		
+		JLabel lblNewLabel_3 = new JLabel("设备类型：");
+		GridBagConstraints gbc_lblNewLabel_3 = new GridBagConstraints();
+		gbc_lblNewLabel_3.anchor = GridBagConstraints.EAST;
+		gbc_lblNewLabel_3.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel_3.gridx = 5;
+		gbc_lblNewLabel_3.gridy = 4;
+		contentPane.add(lblNewLabel_3, gbc_lblNewLabel_3);
+		
+		textField_6 = new JTextField();
+		GridBagConstraints gbc_textField_6 = new GridBagConstraints();
+		gbc_textField_6.insets = new Insets(0, 0, 5, 0);
+		gbc_textField_6.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField_6.gridx = 6;
+		gbc_textField_6.gridy = 4;
+		contentPane.add(textField_6, gbc_textField_6);
+		textField_6.setColumns(10);
+		
+		JLabel lblNewLabel_5 = new JLabel("产品寿命：");
+		GridBagConstraints gbc_lblNewLabel_5 = new GridBagConstraints();
+		gbc_lblNewLabel_5.anchor = GridBagConstraints.EAST;
+		gbc_lblNewLabel_5.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel_5.gridx = 0;
+		gbc_lblNewLabel_5.gridy = 5;
+		contentPane.add(lblNewLabel_5, gbc_lblNewLabel_5);
+		
+		textField_2 = new JTextField();
+		GridBagConstraints gbc_textField_2 = new GridBagConstraints();
+		gbc_textField_2.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField_2.gridwidth = 3;
+		gbc_textField_2.insets = new Insets(0, 0, 5, 5);
+		gbc_textField_2.gridx = 1;
+		gbc_textField_2.gridy = 5;
+		contentPane.add(textField_2, gbc_textField_2);
+		textField_2.setColumns(10);
+		
+		JLabel lblNewLabel_4 = new JLabel("开始日期：");
+		GridBagConstraints gbc_lblNewLabel_4 = new GridBagConstraints();
+		gbc_lblNewLabel_4.anchor = GridBagConstraints.EAST;
+		gbc_lblNewLabel_4.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel_4.gridx = 5;
+		gbc_lblNewLabel_4.gridy = 5;
+		contentPane.add(lblNewLabel_4, gbc_lblNewLabel_4);
+		
+		textField_7 = new JTextField();
+		GridBagConstraints gbc_textField_7 = new GridBagConstraints();
+		gbc_textField_7.insets = new Insets(0, 0, 5, 0);
+		gbc_textField_7.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField_7.gridx = 6;
+		gbc_textField_7.gridy = 5;
+		contentPane.add(textField_7, gbc_textField_7);
+		textField_7.setColumns(10);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		GridBagConstraints gbc_scrollPane = new GridBagConstraints();
+		gbc_scrollPane.insets = new Insets(0, 0, 5, 0);
+		gbc_scrollPane.gridwidth = 7;
+		gbc_scrollPane.fill = GridBagConstraints.BOTH;
+		gbc_scrollPane.gridx = 0;
+		gbc_scrollPane.gridy = 6;
+		contentPane.add(scrollPane, gbc_scrollPane);
+		table = new JTable();
+		table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		table.setCellSelectionEnabled(true);
+		table.setColumnSelectionAllowed(true);
+		table.setModel(new DefaultTableModel(
+			new Object[][] {
+				{null, null, null},
+				{null, null, null},
+				{null, null, null},
+			},
+			new String[] {
+				" ", "\u5C5E\u6027\u540D", "\u53C2\u8003\u503C"
+			}
+		));
+		table.getColumnModel().getColumn(1).setPreferredWidth(86);
+		scrollPane.setViewportView(table);
+		
+		btnNewButton = new JButton("\u6DFB\u52A0\u5C5E\u6027");
+		GridBagConstraints gbc_btnNewButton = new GridBagConstraints();
+		gbc_btnNewButton.anchor = GridBagConstraints.WEST;
+		gbc_btnNewButton.gridwidth = 2;
+		gbc_btnNewButton.insets = new Insets(0, 0, 0, 5);
+		gbc_btnNewButton.gridx = 1;
+		gbc_btnNewButton.gridy = 7;
+		contentPane.add(btnNewButton, gbc_btnNewButton);
+		
+		button = new JButton("\u5220\u9664\u5C5E\u6027");
+		GridBagConstraints gbc_button = new GridBagConstraints();
+		gbc_button.anchor = GridBagConstraints.WEST;
+		gbc_button.gridwidth = 2;
+		gbc_button.insets = new Insets(0, 0, 0, 5);
+		gbc_button.gridx = 4;
+		gbc_button.gridy = 7;
+		contentPane.add(button, gbc_button);
+		
+		button_1 = new JButton("\u786E\u8BA4");
+		GridBagConstraints gbc_button_1 = new GridBagConstraints();
+		gbc_button_1.gridx = 6;
+		gbc_button_1.gridy = 7;
+		contentPane.add(button_1, gbc_button_1);
+
+	}
+
+}
