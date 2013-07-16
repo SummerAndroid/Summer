83bd7f2581ef9028e22acb0a46c84adee46a64c6
ZhangjunIris <Zh920J@163.com>
2013-07-16 15:31:44 +0800

    i

--- Summer-Android/src/summer/android/HistoryTaskLook.java
+++ Summer-Android/src/summer/android/HistoryTaskLook.java
@@ -30,18 +30,23 @@ import summer.inf.I.Req;
 /**
  * @author ZhangJun
  */
-
-public class TaskletPull extends ListActivity {
+public class HistoryTaskLook extends ListActivity {
 	int length;
 	User user;
-	Tasklet[] tasklet;//传递tasklet对象
+	Tasklet[] tasklet;// 传递tasklet对象
+	Long start, end;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.taskletpull);
+		setContentView(R.layout.history_task_look);
 		Intent intent = getIntent();
 		user = (User) intent.getSerializableExtra("user");
+		start = intent.getLongExtra("start", 0L);
+		Log.i("开始时间!!!!!!!!!!", start + "");
+		end = intent.getLongExtra("end", 0L);
+		Log.i("结束时间!!!!!!!!!!", end + "");
 		Handler handler = new Handler() {
 			@Override
 			public void handleMessage(Message msg) {
@@ -54,9 +59,9 @@ public class TaskletPull extends ListActivity {
 					ArrayList<HashMap<String, String>> list = new ArrayList<HashMap<String, String>>();
 					List<Tasklet> list1 = (List<Tasklet>) msg.obj;
 					length = list1.size();
-					tasklet=new Tasklet[length];
+					tasklet = new Tasklet[length];
 					for (int i = 0; i < length; i++) {
-						tasklet[i]=list1.get(i);
+						tasklet[i] = list1.get(i);
 						String taskletName = list1.get(i).getName();
 						// HashMpa为键值对类型。第一个参数为建，第二个参数为值
 						HashMap<String, String> map = new HashMap<String, String>();
@@ -66,7 +71,8 @@ public class TaskletPull extends ListActivity {
 					}
 					// 生成一个SimpleAdapter类型的变量来填充数据
 					SimpleAdapter listAdapter = new SimpleAdapter(
-							TaskletPull.this, list, R.layout.taskletpullitem,
+							HistoryTaskLook.this, list,
+							R.layout.taskletpullitem,
 							new String[] { "device" },
 							new int[] { R.id.device });
 					// 设置显示ListView
@@ -82,25 +88,8 @@ public class TaskletPull extends ListActivity {
 		};
 		TaskletPullUtil pullUtil = new TaskletPullUtil(handler);
 		pullUtil.taskletPull(user, Long.valueOf(Req.TASKLET_ALL),
-				Long.valueOf(Req.TASKLET_ALL), false,
+				Long.valueOf(Req.TASKLET_ALL), true,
 				Req.TASKLET_LIST_ORDER_TIME_DES);
-
-	}
-
-	// 重写onListItemClick但是ListView条目事件
-	@Override
-	protected void onListItemClick(ListView l, View v, int position, long id) {
-		// TODO Auto-generated method stub
-		super.onListItemClick(l, v, position, id);
-		for (int i = 0; i < length; i++) {
-			if (id == i) {
-				// 转移到设备界面
-				Intent intent = new Intent();
-				intent.putExtra("tasklet", tasklet[i]);
-				intent.setClass(TaskletPull.this, TaskletItemPull.class);
-				TaskletPull.this.startActivity(intent);
-			}
-		}
 	}
 
 }
